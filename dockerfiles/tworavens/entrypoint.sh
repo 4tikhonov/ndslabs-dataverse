f15a4f61efa75dd404630943b908c542d35349a6
Craig Willis <willis8@illinois.edu>
2016-09-23 20:55:57 -0500

    Added TWORAVENS_URL

--- dockerfiles/tworavens/entrypoint.sh
+++ dockerfiles/tworavens/entrypoint.sh
@@ -9,8 +9,16 @@ if [ "$1" = 'tworavens' ]; then
 	echo You must specify DATAVERSE_URL
 	exit 1
   fi
+  o
+  if [ -z "$TWORAVENS_URL" ]; then
+	echo You must specify TWORAVENS_URL
+	exit 1
+  fi
 
-  echo "Starting TwoRavens with DATAVERSE_URL=$DATAVERSE_URL"
+  echo "Starting TwoRavens"
+  echo "DATAVERSE_URL=$DATAVERSE_URL"
+  echo "TWORAVENS_URL=$TWORAVENS_URL"
+
   /start-tworavens
   httpd -DFOREGROUND
 else
